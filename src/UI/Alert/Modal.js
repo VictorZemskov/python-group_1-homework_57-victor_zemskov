d3c08989d7853af4728e521b12932d19cc9536c5
Victor <vzemskov090583@gmail.com>
2019-02-18 13:17:47 +0600

    Added Alert

---
+++ src/UI/Alert/Modal.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Modal.css';
+import Backdrop from "../Backdrop/Backdrop";
+import Button from "../Button/Button";
+
+const Modal = props => (
+    <div>
+      <Backdrop show={props.show} cancel={props.cancel}/>
+      <div className={"Modal" + (props.show ? ' Modal-show' : '')}>
+          <h2>{props.title}</h2>
+          <Button btnType="Danger" click={props.closed}>CANCEL</Button>
+        {props.children}
+      </div>
+    </div>
+);
+
+export default Modal;
