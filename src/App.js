f8f4917b9b1dc05834094d8c3c80e8438d100937
Victor <vzemskov090583@gmail.com>
2019-02-18 13:17:34 +0600

    Added Alert

--- src/App.js
+++ src/App.js
@@ -4,13 +4,22 @@ import './App.css';
 import Modal from "./UI/Modal/Modal";
 
 class App extends Component {
-  state = {purchasable: false, purchasing: false,};
+  state = {purchasable: false, modalShow: true,};
 
+
+
+  closed = () => {
+    this.setState({modalShow: false});
+  };
 
   render() {
     return (
       <div className="App">
-          <Modal>
+          <Modal
+              show={this.state.modalShow}
+              closed={this.closed}
+              title="Some kinda modal title"
+          >
             <p>This is modal content</p>
           </Modal>
 
--- src/UI/Button/Button.js
+++ src/UI/Button/Button.js
@@ -3,7 +3,7 @@ import './Button.css';
 
 const Button = props => (
   <button
-    onClick={props.clicked}
+    onClick={props.click}
     className={['Button', props.btnType].join(' ')}
   >
     {props.children}
