aaf744ce7daf979cf3d120dcddb69036b9b0403d
Victor <vzemskov090583@gmail.com>
2019-02-18 12:40:16 +0600

    Added UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Modal from "./UI/Modal/Modal";
 
 class App extends Component {
+  state = {purchasable: false, purchasing: false,};
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <Modal>
+            <p>This is modal content</p>
+          </Modal>
+
       </div>
     );
   }
